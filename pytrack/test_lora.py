8a3c44f3e1e1dcef471b57089fc8b4da11a8095a
Christopher Wadden <christopherwadden@cvsdvt.org>
2026-05-05 22:33:16 -0400

    fix totally uniportant dollar signs

--- pytrack/test_lora.py
+++ pytrack/test_lora.py
@@ -9,7 +9,7 @@ mylora = LoRa(Channel=0, Frequency=915.0, Mode=1, DIO0=4)
 while True:
     print("Send message")
     now = int(time.time())
-    mylora.send_text(f"$$Mr. Watson, come here; I want you - {now}\n")
+    mylora.send_text(f"Mr. Watson, come here; I want you - {now}\n")
 
     while mylora.is_sending():
 	    time.sleep(0.01)
